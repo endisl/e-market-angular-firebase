a06c3a1b479e18bdbee930b2e34c83142a4befc9
endisl <endiluamba@gmail.com>
2022-02-25 19:25:32 -0300

    Configured Bootstrap and Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAEYGH6sO5pv8iF3rFYw40ECAVlNNH0uAU",
+    authDomain: "emarket-b1cd2.firebaseapp.com",
+    projectId: "emarket-b1cd2",
+    storageBucket: "emarket-b1cd2.appspot.com",
+    messagingSenderId: "738103637941",
+    appId: "1:738103637941:web:a851e2745e5615bd76cbfc"
+  }
 };
 
 /*
