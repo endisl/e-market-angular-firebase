f9a9889d47ea0ee93ed0ee347d00fcbfb7a7e381
endisl <endiluamba@gmail.com>
2022-02-26 18:11:43 -0300

    Implemented Google login and logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user$: Observable<firebase.User | any>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
 
-  ngOnInit(): void {
+  logout() {
+    this.afAuth.signOut();
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/auth'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
 
-  ngOnInit(): void {
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.GoogleAuthProvider());
   }
 
 }
