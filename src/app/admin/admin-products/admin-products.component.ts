c5eedd6c02b2a526c0d86a57099d68ddafcb9afa
endisl <endiluamba@gmail.com>
2022-06-13 00:13:38 -0300

    e-market

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataTableResource } from 'angular-4-data-table';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/product.service';
@@ -12,9 +13,29 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) {
-    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.subscription = this.productService.getAll()
+    .subscribe(products => {
+      this.filteredProducts = this.products = products;
+
+      this.initializeTable(products);
+    });
+  }
+
+  private initializeTable(products: Product[]) {
+      this.tableResource = new DataTableResource(products);
+      this.tableResource.query({ offset: 0 }).then(items => this.items = items);
+      this.tableResource.count().then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params).then(items => this.items = items);
   }
 
   filter(query: string) {
